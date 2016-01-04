0ded62220cf9557c5b195a54bea4043f721bc716
Oscar Ayala <osyala@gmail.com>
2016-01-04 13:20:33 -0600

    first commit

---
+++ Javascript/script.js
@@ -0,0 +1,12 @@
+// es5 and 6 polyfills, powered by babel
+require("babel/polyfill")
+
+let fetch = require('./fetcher'),
+    React = require('react'),
+    $ = require('jquery'),
+    Backbone = require('backbone')
+
+
+
+    
+console.log("Javascript loaded!")
